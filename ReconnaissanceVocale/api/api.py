6a3145f450597fccf5e1e7d6faf18dce52186e23
atLuka <luka.samac@outlook.fr>
2024-12-06 14:45:19 +0100

    ajout du endpoint

---
+++ ReconnaissanceVocale/api/api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, File
+from fastapi.responses import JSONResponse
+import vosk
+from Model.VoxStt import reconnaissance_vocale
+
+# Initialisation de l'application FastAPI
+app = FastAPI()
+
+# Charger le modèle Vosk une seule fois
+model_address = "../../SttVoxModel/vosk-model-small-fr-0.22"
+model = vosk.Model(model_address)
+
+@app.post("/recognize-bytes")
+async def recognize_audio_bytes(audio: bytes = File(...)):
+    """
+    Endpoint pour reconnaître la parole à partir de bytes audio.
+
+    Args:
+        audio (bytes): Données audio en bytes.
+
+    Returns:
+        JSON: Texte reconnu.
+    """
+    try:
+        # Charger les bytes dans un fichier WAV en mémoire
+        texte = reconnaissance_vocale(model,audio)
+        return {"output": texte.strip()}
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": f"Erreur lors du traitement de l'audio : {str(e)}"}
+        )
