e47df263ba78b45a08d945467c4b2bcc39bc173a
KAFAGY <ali.kafagy@vws.vh20.net>
2024-12-09 15:29:24 +0100

    Correction de la compréhension de l'audio (Plus de teste nécessaire)

--- ReconnaissanceVocale/api/Model/VoxStt.py
+++ ReconnaissanceVocale/api/Model/VoxStt.py
@@ -1,46 +1,37 @@
-import vosk
 import json
 import io
 import wave
-
 from starlette.responses import JSONResponse
+from vosk import KaldiRecognizer
 
 
 
-def reconnaissance_vocale(model,audio):
+def reconnaissance_vocale(model, audio):
     """
     Traite un chunk audio (en bytes) et effectue la reconnaissance vocale.
 
     Args:
-        audio_bytes (bytes): Données audio en bytes.
+        audio (bytes): Données audio en bytes.
 
     Returns:
         str: Le texte reconnu, ou une chaîne vide si aucune reconnaissance complète.
     """
     audio_stream = io.BytesIO(audio)
     wf = wave.open(audio_stream, "rb")
-
-    # Vérifier les caractéristiques du fichier audio
     if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getframerate() != 16000:
         return JSONResponse(
             status_code=400,
             content={"error": "Le fichier audio doit être mono WAV 16kHz avec une profondeur de 16 bits"}
         )
-
-    # Initialiser le recognizer avec vosk (réutilise l'instance de modèle)
-    recognizer = vosk.KaldiRecognizer(model, wf.getframerate())
-
-    # Lecture des frames audio et reconnaissance vocale
-    texte = ""
+    recognizer = KaldiRecognizer(model, wf.getframerate())
     while True:
         data = wf.readframes(4000)
         if len(data) == 0:
             break
-        if recognizer.AcceptWaveform(data):
-            result = json.loads(recognizer.Result())
-            texte += result.get("text", "") + " "
-
-    # Fermer le fichier wave
+        recognizer.AcceptWaveform(data)
+    final_result = json.loads(recognizer.FinalResult())
+    texte = final_result.get("text", "")
     wf.close()
-    return texte
+
+    return texte.strip()
 
